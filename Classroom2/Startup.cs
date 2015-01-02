a3d2dbcffc2b78103b93a99a8865a458367f3a46
AxelVerstappen <axel_verstappen@hotmail.com>
2015-01-02 17:37:25 +0100

    second commit

---
+++ Classroom2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Classroom2.Startup))]
+namespace Classroom2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
