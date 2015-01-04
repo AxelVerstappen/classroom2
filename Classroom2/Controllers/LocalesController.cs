371c7555f02ca2ca5ebde0bae4eb26e43d29eeeb
Thomas <thomas.d.bock@gmail.com>
2015-01-04 12:29:14 +0100

    merge fixes

--- Classroom2/Controllers/LocalesController.cs
+++ Classroom2/Controllers/LocalesController.cs
@@ -10,27 +10,15 @@ namespace Classroom2.Controllers
         public ActionResult Index(string lang = "en_US")
         {
             Response.Cookies["CacheLang"].Value = lang;
-<<<<<<< HEAD
 
             if (Request.UrlReferrer != null)
                 Response.Redirect(Request.UrlReferrer.ToString());
 
 			var message = Localization.Get("changedlng");
 
-=======
-
-            if (Request.UrlReferrer != null)
-                Response.Redirect(Request.UrlReferrer.ToString());
-
-			var message = Localization.Get("changedlng");
-
->>>>>>> origin/master
 			return Content(message);
         }
 
     }
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> origin/master
