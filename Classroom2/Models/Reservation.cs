7550e0db9fefb9f2db120b38b0ed9a876d8831d8
AxelVerstappen <axel_verstappen@hotmail.com>
2015-01-04 14:35:56 +0100

    Reservation model

---
+++ Classroom2/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Classroom2.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string TeacherName { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int ClassroomId { get; set; }
+    }
+}
